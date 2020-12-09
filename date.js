71bd83c80b6185dd5ef03ecc24c87d4bf5c5212b
Anisha Jindal <56153334+alphavoid@users.noreply.github.com>
2020-12-09 14:07:31 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,22 @@
+//jshint eversion:6
+
+module.exports.getDate = function() {
+    const today = new Date();
+
+    const options = {
+        weekday: "long",
+        day:"numeric",
+        month:"long"  
+    };
+    return today.toLocaleDateString("en-US", options);
+}
+
+module.exports.getDay = function() {
+    const today = new Date();
+
+    const options = {
+        weekday: "long",  
+    };
+    return today.toLocaleDateString("en-US", options);
+    
+}
